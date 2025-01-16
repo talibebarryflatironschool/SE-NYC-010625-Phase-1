00789689fed145c133c116752aa39dbdc6add4d3
Ricardo Guerra <RikkuG491@gmail.com>
2025-01-16 16:05:51 -0500

    Created starter code and content for Lecture 9

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const restaurantMenu = document.getElementById('restaurant-menu')
+const foodDetailImageElement = document.querySelector('.detail-image')
+const foodNameElement = document.querySelector('.name')
+const foodDescriptionDisplayElement = document.getElementById('description-display')
+
+function addFoodImageToRestaurantMenu(food){
+    const imgElement = document.createElement('img')
+    imgElement.src = food.image
+
+    imgElement.addEventListener('mouseover', () => {
+        displayFoodDetails(food)
+    })
+
+    imgElement.addEventListener('click', () => {
+        fetch(`http://localhost:3000/foods/${food.id}`, {
+            method: "DELETE"
+        })
+        .then(response => {
+            if(response.ok){
+                imgElement.remove()
+            }
+            else{
+                alert(`Error: Unable to delete Food # ${food.id}`)
+            }
+        })
+    })
+
+    restaurantMenu.appendChild(imgElement)
+}
+
+function displayFoodDetails(food){
+    foodDetailImageElement.src = food.image
+    foodNameElement.textContent = food.name
+    foodDescriptionDisplayElement.textContent = food.description
+}
+
+fetch('http://localhost:3000/foods')
+.then(response => response.json())
+.then(foods => {
+    displayFoodDetails(foods[0])
+
+    foods.forEach(addFoodImageToRestaurantMenu)
+})
+
+// write your code here
